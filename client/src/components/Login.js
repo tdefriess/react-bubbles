c1587d2154d647439bd553c03813f2763cf024f7
Tyler de Friess <tyler.defriess@gmail.com>
2020-02-28 11:29:02 -0500

    Built login page, sign in post request working, receiving access token. PrivateRoute built

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', credentials)
+      .then(res => {
+        console.log('Login:', res.data.payload);
+        window.localStorage.setItem('Token', res.data.payload);
+      })
+      .catch(err => console.log('Login error:', err));
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='username'
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='password'
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log In</button>
+      </form>
     </>
   );
 };
